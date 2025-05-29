93b2aa83bec49ec90fa134ba21e84fef946af6c4
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-29 12:16:05 +0000

    made a functioning pad for the etchasketch

--- javascript.js
+++ javascript.js
@@ -4,12 +4,24 @@ const pad = document.getElementById("pad");
 
 function makeCanvas(pixel) {
     for (let i = 0; i < pixel; i++) {
-        const button = document.createElement(`button${i}`);
-        button.addEventListener('click', function () {
-            button.style.backgroundColor = "black";
+        const vertical = document.createElement(`div${i}`);
+        vertical.style.display = "flex";
+        vertical.style.flexDirection = "column";
+        vertical.style.flex = `1`;
+        vertical.style.aspectRatio = "1 / 1";
+        pad.appendChild(vertical);
+        for (let j = 0; j < pixel; j++) {
+            const horizontal = document.createElement(`div${j}`);
+            horizontal.margin = "0";
+            horizontal.style.flex = `1`;
+            horizontal.style.border = "1px solid blue";
+            horizontal.addEventListener('mouseover', function () {
+            horizontal.style.backgroundColor = "black";
         });
-        pad.appendChild(button);
+            vertical.appendChild(horizontal);
+        }
     }
+
 };
 
-makeCanvas(16);
+makeCanvas(9);

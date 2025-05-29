70df2ba45781870d5ec7fad8e1acacef7c3d6030
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-29 22:15:52 +0000

    changed some aspect of the pad. added sidebar

--- javascript.js
+++ javascript.js
@@ -2,21 +2,22 @@
 const pad = document.getElementById("pad");
 
 
-function makeCanvas(pixel) {
-    for (let i = 0; i < pixel; i++) {
+function makeCanvas(pixelNum) {
+    for (let i = 0; i < pixelNum; i++) {
         const vertical = document.createElement(`div${i}`);
         vertical.style.display = "flex";
         vertical.style.flexDirection = "column";
         vertical.style.flex = `1`;
-        vertical.style.aspectRatio = "1 / 1";
         pad.appendChild(vertical);
-        for (let j = 0; j < pixel; j++) {
+        for (let j = 0; j < pixelNum; j++) {
             const horizontal = document.createElement(`div${j}`);
             horizontal.margin = "0";
             horizontal.style.flex = `1`;
             horizontal.style.border = "1px solid blue";
-            horizontal.addEventListener('mouseover', function () {
-            horizontal.style.backgroundColor = "black";
+            horizontal.id = "pixel";
+            
+            horizontal.addEventListener('click', function () {
+            horizontal.style.opacity = 1;
         });
             vertical.appendChild(horizontal);
         }
@@ -24,4 +25,4 @@ function makeCanvas(pixel) {
 
 };
 
-makeCanvas(9);
+makeCanvas(7);

30d1459ea3c5ea8f5fdb4dea409cd69ca4682dd8
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-30 10:46:28 +0000

    enabled color buttons to be used

--- javascript.js
+++ javascript.js
@@ -3,22 +3,61 @@ let brush = "black";
 
 // DOM 
 const pad = document.getElementById("pad");
+
 const sixteen = document.getElementById("scaleOne");
 const threeTwo = document.getElementById("scaleTwo");
 const sixfour = document.getElementById("scaleThree");
+
+const black = document.getElementById("black");
+const red = document.getElementById("red");
+const blue = document.getElementById("blue");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+const orange = document.getElementById("orange");
+
 const eraser = document.getElementById("eraser");
 
 // buttons
+// 16x16
 sixteen.addEventListener('click', function() {
     clearCanvas();
     makeCanvas(16);});
+// 32x32
 threeTwo.addEventListener('click', function() {
     clearCanvas();
     makeCanvas(32)});
+// 64x64
 sixfour.addEventListener('click', function() {
     clearCanvas();
     makeCanvas(64)});
+// black
+black.addEventListener('click', function() {
+    brush = "black";
+});
+// red
+red.addEventListener('click', function() {
+    brush = "red";
+});
+// blue
+blue.addEventListener('click', function() {
+    brush = "blue";
+});
+// yellow
+yellow.addEventListener('click', function() {
+    brush = "yellow";
+});
+// green 
+green.addEventListener('click', function() {
+    brush = "green";
+});
+// orange
+orange.addEventListener('click', function() {
+    brush = "orange";
+});
+
+// eraser
 eraser.addEventListener('click', function() {
+    clearCanvas();
     makeCanvas(3)});
 
 // functions
@@ -39,7 +78,7 @@ function makeCanvas(pixelNum) {
             pixel.style.border = "1px solid green";
             pixel.id = "pixel";
             pixel.addEventListener('click', function () {
-                pixel.style.backgroundColor = "black";
+                pixel.style.backgroundColor = brush;
             });
             vertical.appendChild(pixel);
         }

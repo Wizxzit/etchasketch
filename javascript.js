e8720192ecb6f3d92000a8bdeb5529c32c64935c
Wisit Tangtakoune <wisit.tangtakoune@gmail.com>
2025-05-30 07:30:08 +0000

    added a function to clear the sketch board

--- javascript.js
+++ javascript.js
@@ -1,7 +1,28 @@
+// variables
+let brush = "black";
+
 // DOM 
 const pad = document.getElementById("pad");
+const sixteen = document.getElementById("scaleOne");
+const threeTwo = document.getElementById("scaleTwo");
+const sixfour = document.getElementById("scaleThree");
+
+// buttons
+sixteen.addEventListener('click', a => {
+    clearCanvas();
+    makeCanvas(16);});
+threeTwo.addEventListener('click', a => {
+    clearCanvas();
+    makeCanvas(32)});
+sixfour.addEventListener('click', a => {
+    clearCanvas();
+    makeCanvas(64)});
 
 
+// functions
+// makeCanvas adds pixels to the sketch board according to the input number
+// parameter (a => a*a). It makes a-number of vertical containers and fills
+// the container with a-elements. 
 function makeCanvas(pixelNum) {
     for (let i = 0; i < pixelNum; i++) {
         const vertical = document.createElement(`div${i}`);
@@ -10,19 +31,22 @@ function makeCanvas(pixelNum) {
         vertical.style.flex = `1`;
         pad.appendChild(vertical);
         for (let j = 0; j < pixelNum; j++) {
-            const horizontal = document.createElement(`div${j}`);
-            horizontal.margin = "0";
-            horizontal.style.flex = `1`;
-            horizontal.style.border = "1px solid blue";
-            horizontal.id = "pixel";
-            
-            horizontal.addEventListener('click', function () {
-            horizontal.style.opacity = 1;
-        });
-            vertical.appendChild(horizontal);
+            let pixel = document.createElement(`div${j}`);
+            pixel.margin = "0";
+            pixel.style.flex = `1`;
+            pixel.style.border = "1px solid green";
+            pixel.id = "pixel";
+            pixel.addEventListener('click', function () {
+                pixel.style.backgroundColor = "white";
+            });
+            vertical.appendChild(pixel);
         }
     }
-
 };
 
-makeCanvas(7);
+function clearCanvas() {
+    pad.innerHTML = "";
+};
+
+
+makeCanvas(3);
